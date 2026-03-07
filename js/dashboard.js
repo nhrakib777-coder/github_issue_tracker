0f2501698f2dc6c95c43febc067f856ea8a310a1
nhrakib777-coder <rakibnh93@gmail.com>
2026-03-07 22:31:51 +0800

    login page setup

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,4 @@
-console.log("Login page is working");
+// check if user is logged in or not, if not then redirect to login page
+if(localStorage.getItem('isLoggedIn') !== 'true') {
+    window.location.href = 'index.html';
+}
--- js/login.js
+++ js/login.js
@@ -1,1 +1,32 @@
-console.log("Login page loaded");
+// Login page JavaScript
+// geting elements
+const userName = document.getElementById('userName');
+const passWord = document.getElementById('password');
+const signInBtn = document.getElementById('signInBtn');
+
+// handle login function
+const handleLogin = () => {
+  // get inpur value
+  const userNameValue = userName.value;
+  const passWordValue = passWord.value;
+// validation
+  if (userNameValue === '' || passWordValue === '') {
+    alert('Please fill in both username and password fields.');
+    return;
+  }
+  if (userNameValue !== 'admin') {
+    alert('Invalid username. please try again');
+    return;
+  }
+  if (passWordValue !== 'admin123') {
+    alert('Invalid Password. please try again');
+    return;
+  }
+  // set login status in local storage
+  localStorage.setItem('isLoggedIn', 'true');
+  // redirect to dashboard page
+  window.location.href = '../dashboard.html';
+};
+
+// add eventListener to sign in button
+signInBtn.addEventListener('click', handleLogin);
